48f695af8340483c50cfea00e4ce4d46ea1e3301
Joshua Patterson <jleegippies@gmail.com>
2020-05-10 19:32:45 -0500

    Smooshed Morse Easy

---
+++ smooshed_morse.py
@@ -0,0 +1,13 @@
+
+alpha_list = 'abcdefghijklmnopqrstuvwxyz'
+morse_list = '.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..'.split()
+alpha_morse_map = dict(zip(alpha_list, morse_list))
+
+
+def morsify_str(alpha_str):
+    return ''.join([alpha_morse_map[c] for c in alpha_str])
+
+
+if __name__ == '__main__':
+    print(morsify_str('sos'))
+    print(morsify_str('helloworld'))
